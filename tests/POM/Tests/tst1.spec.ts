2c24393fe9e65f283f91a8103d16857f6d92dd36
VladimirGera <vladimir.v.gera@gmail.com>
2025-11-08 12:23:01 +0300

    add fixtures

--- tests/POM/Tests/tst1.spec.ts
+++ tests/POM/Tests/tst1.spec.ts
@@ -1,43 +1,36 @@
-import { test, expect, Page, Locator } from '@playwright/test';
+import { test, expect } from '../../fixtures/mainPage';
 import { MainPage } from '../Pages/MainPage';
 
-test.describe('Тесты главной страницы', () => {
-  let mainPage: MainPage;
-  test.beforeEach(async ({ page }) => {
-    mainPage = new MainPage(page);
-    await mainPage.openMainPage();
+test('Проверка отображения элементов навигации хедера', async ({ mainPage }) => {
+  await mainPage.checkElementVisability();
+});
+test('Проверка названия элементов навигации хедера', async ({ mainPage }) => {
+  await mainPage.checkTitlesOfElements();
+});
+test('Проверка атрибутов href элементов навигации хедера', async ({ mainPage }) => {
+  await mainPage.checkAttributesHref();
+});
+test('Проверка переключения light mode', async ({ mainPage }) => {
+  test.step('Нажатие на иконку переключения light mode', async () => {
+    await mainPage.clickLightMode();
   });
-  test('Проверка отображения элементов навигации хедера', async () => {
-    await mainPage.checkElementVisability();
+  test.step('Проверка смены атрибута', async () => {
+    await mainPage.checkAttributeMode();
   });
-  test('Проверка названия элементов навигации хедера', async () => {
-    await mainPage.checkTitlesOfElements();
+});
+test('Проверка отображения стиля light', async ({ mainPage }) => {
+  await test.step('Установка светлой темы', async () => {
+    await mainPage.setLightMode();
   });
-  test('Проверка атрибутов href элементов навигации хедера', async () => {
-    await mainPage.checkAttributesHref();
+  await test.step('Скриншотная проверка светлой темы', async () => {
+    await mainPage.checkLayoutWithLightMode();
   });
-  test('Проверка переключения light mode', async () => {
-    test.step('Нажатие на иконку переключения light mode', async () => {
-      await mainPage.clickLightMode();
-    });
-    test.step('Проверка смены атрибута', async () => {
-      await mainPage.checkAttributeMode();
-    });
+});
+test('Проверка отображения стиля dark', async ({ mainPage }) => {
+  await test.step('Установка темной темы', async () => {
+    await mainPage.setDarkMode();
   });
-  test('Проверка отображения стиля light', async () => {
-    await test.step('Установка светлой темы', async () => {
-      await mainPage.setLightMode();
-    });
-    await test.step('Скриншотная проверка светлой темы', async () => {
-      await mainPage.checkLayoutWithLightMode();
-    });
-  });
-  test('Проверка отображения стиля dark', async () => {
-    await test.step('Установка темной темы', async () => {
-      await mainPage.setDarkMode();
-    });
-    await test.step('Скриншотная проверка темной темы', async () => {
-      await mainPage.checkLayoutWithDarkMode();
-    });
+  await test.step('Скриншотная проверка темной темы', async () => {
+    await mainPage.checkLayoutWithDarkMode();
   });
 });
